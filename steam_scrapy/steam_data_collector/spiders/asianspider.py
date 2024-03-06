481230f9a2ddfb42c1d852521185940dcf152e58
Seok Yim(Noah) <seokyim8@gmail.com>
2024-03-05 20:46:16 -0500

    Confirmed that fetching/storing html data works

--- steam_scrapy/steam_data_collector/spiders/asianspider.py
+++ steam_scrapy/steam_data_collector/spiders/asianspider.py
@@ -1,15 +1,17 @@
 from typing import Iterable
 import scrapy
+from pathlib import Path
 
 
 class AsianspiderSpider(scrapy.Spider):
     name = "asian"
     allowed_domains = ["store.steampowered.com"]
     urls = ["https://store.steampowered.com/explore/new/"]
+    save_file = "steam_new_releases.html"
 
     def start_requests(self) -> Iterable[scrapy.Request]:
         for url in self.urls:
             yield scrapy.Request(url = url, callback = self.parse)
 
     def parse(self, response):
-        print(response)
+        Path(self.save_file).write_bytes(response.body)

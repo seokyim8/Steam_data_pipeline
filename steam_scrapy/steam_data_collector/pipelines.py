ed150bd9fc2882270af70993e179cf367151f795
Seok Yim(Noah) <seokyim8@gmail.com>
2024-03-08 10:15:12 -0500

    Commiting .gitignore

--- steam_scrapy/steam_data_collector/pipelines.py
+++ steam_scrapy/steam_data_collector/pipelines.py
@@ -4,10 +4,22 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import mysql.connector
+import secrets
 
 
 class SteamDataCollectorPipeline:
+    """Initializes connection to the AWS RDS MYSQL instance."""
+    def open_spider(self, spider):
+        # self.db = mysql.connector.connect(
+        #     host = secrets.__path__,
+        #     user = secrets.__path__,
+        #     password = secrets.__path__ 
+        # )
+        # self.db.disconnect()
+        pass
+
+    """Process the scraped data from Steam and upsert to the MYSQL table."""
     def process_item(self, item, spider):
         return item

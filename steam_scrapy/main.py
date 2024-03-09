2df0ae02326d99220aef5c0cb1e28b470a6f488b
Seok Yim(Noah) <seokyim8@gmail.com>
2024-03-09 17:33:55 -0500

    Added scheduling functionality

--- steam_scrapy/main.py
+++ steam_scrapy/main.py
@@ -4,14 +4,31 @@
 import time
 import os
 import subprocess
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import asyncio
 from datetime import date, datetime
 
 SPIDER_NAME = 'asian'
 
-def run_scraper():
-    """Runs the spider to scrape from Steam the data relating to newly released games."""
-    subprocess.run(['scrapy', 'crawl', SPIDER_NAME])
+
+def schedule_scraping():
+    """Schedules the daily scraping process from Steam for the purpose of collecting new-releases related data."""
+    def run_scraper():
+        subprocess.run(['scrapy', 'crawl', SPIDER_NAME])
+    
+    scheduler = AsyncIOScheduler()
+    # At 7:30 am every morning, the RDS MYSQL instance will be updated with info pertaining to the newly released steam games.
+    scheduler.add_job(func = run_scraper, 
+                      trigger = CronTrigger(year = "*", month = "*", day = "*", hour = "7", minute = "30"))
+    scheduler.start()
+
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
 
 # Main Function
 if __name__ == "__main__":
-    run_scraper()
+    print("Successfully started running server.")
+    schedule_scraping()

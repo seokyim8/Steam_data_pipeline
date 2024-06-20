2010686b4319c973b8ab8fdb38b9b6870e2b705f
Seok Yim(Noah) <seokyim8@gmail.com>
2024-06-20 13:17:41 -0400

    Add webscraping option to prevent crashing

--- steam_scrapy/steam_data_collector/spiders/asianspider.py
+++ steam_scrapy/steam_data_collector/spiders/asianspider.py
@@ -27,6 +27,7 @@ class AsianspiderSpider(scrapy.Spider):
         options.add_argument('--headless')
         options.add_argument('--profile-directory=Default')
         options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage') # Added on Jun 20th, 2024 to prevent crawler from crashing
 
         self.driver = webdriver.Chrome(options = options)
 
